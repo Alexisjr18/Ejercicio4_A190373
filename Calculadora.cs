8b6466e8a42a8037b431581be7bb28bb811655b7
Alexis García <Alexis Garca@DESKTOP-9AO2PNB>
2023-09-17 17:32:08 -0600

    Agregar archivos de proyecto.

---
+++ Calculadora.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calculadora
+{
+	public Class Calculadora
+	{
+		public decimal base1 { get; set }
+	    public decimal altura { get; set }
+	public decimal Calculadora(decimal base1, decimal altura)
+    {
+		return (base1 * altura) / 2;
+    }
+
+	public decimal Calculadora(decimal lado)
+    {
+		return lado * lado;
+    }
+
+	public decimal Calculadora(double radio)
+    {
+		double area = Math.PI;
+		return (decimal)(area * radio * radio);
+    }
+
+	}
+}
---
+++ CalculadoraWS/CalculadoraWebService.asmx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace CalculadoraWS
+{
+    /// <summary>
+    /// Descripción breve de CalculadoraWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CalculadoraWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string WELCOME()
+        {
+            return "Bienvenido";
+        }
+
+
+        [WebMethod]
+        public decimal Area_De_Triangulo(decimal base1, decimal altura)
+        {
+            Calcular area = new Calcular();
+
+            return area.AreaTriangulo(base1, altura);
+        }
+
+        [WebMethod]
+        public decimal Area_De_Cuadrado(decimal lado)
+        {
+            Calcular area = new Calcular();
+
+            return area.AreaCuadrado(lado);
+        }
+
+        [WebMethod]
+        public decimal Area_De_Circulo(double radio)
+        {
+            Calcular area = new Calcular();
+            return area.AreaCirculo(radio);
+        }
+    }
+}
